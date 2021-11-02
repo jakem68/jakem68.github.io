f7a33e672c5ea55ce7ee2a723804820c317e5289
Jan Kempeneers <jan.kempeneers2@gmail.com>
2021-11-02 23:19:58 +0100

    function(nav):change active
    change class active to element clicked

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,3 +1,5 @@
+
+
 // Select the button
 const btn = document.querySelector('.btn-toggle');
 
@@ -6,3 +8,17 @@ btn.addEventListener('click', function() {
   // Then toggle (add/remove) the .dark-theme class to the body
   document.body.classList.toggle('dark-theme');  
 })
+
+
+
+$(document).ready(function () {
+  $('.nav a').click(function(e) {
+
+      $('.nav .active').removeClass('active');
+
+      // var $parent = $(this).parent();
+      // $parent.addClass('active');
+      $(this).addClass('active');
+      e.preventDefault();
+  });
+});
