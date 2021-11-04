e8b29ffb114073782eaa6f199cbdf5e1eec117e4
Jan Kempeneers <jan.kempeneers2@gmail.com>
2021-11-04 13:31:46 +0100

    modified nav_load.js and nav.html

--- assets/javascript/nav_load.js
+++ assets/javascript/nav_load.js
@@ -5,12 +5,17 @@ $(document).ready(function () {
   });
 });
 
-// when loaded check which anchor href matches the 
+// when loaded check which anchor href matches the
 // loaded url and make active
+let active_set = false;
 $("#nav-placeholder").on("navReady", function () {
-    $('.nav a').each(function() {  
-      if (window.location.href === $(this).prop("href")){
-        $(this).addClass("active");
-      }
-    });
+  $(".nav a").each(function () {
+    if (window.location.href === $(this).prop("href")) {
+      $(this).addClass("active");
+      active_set = true;
+    }
+  });
+  if (active_set === false) {
+    $(".nav a.default").addClass("active");
+  }
 });
